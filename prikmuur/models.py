da97c1b2947dfa06a80fac97f353c2265cb2d8b2
Jelle Kaufmann <github@pa5am.nl>
2019-06-12 13:20:01 +0200

    Prikmuur naar aparte app verplats

--- prikmuur/models.py
+++ prikmuur/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import Group
+from users.models import CustomUser
 
-# Create your models here.
+class Post(models.Model):
+  created = models.DateTimeField(auto_now_add=True)
+  group = models.ForeignKey(Group, on_delete=models.CASCADE)
+  postedBy = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+  text = models.TextField()
+
+  class Meta:
+    ordering = ('created',)
+
--- prikmuur/views.py
+++ prikmuur/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from .models import Post
+from .serializers import PrikmuurSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PrikmuurSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PrikmuurSerializer
+

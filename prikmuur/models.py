57d70baf39aa2a1e3738321fc9238962bd6a3a2d
HDJRijnders <hylbrenrijnders@hotmail.com>
2019-06-26 12:26:25 +0200

    FK van group nu goed geknoopt

--- prikmuur/models.py
+++ prikmuur/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import Group
 from users.models import CustomUser
+from friendcircle.models import FriendCircle
 
 class Post(models.Model):
   created = models.DateTimeField(auto_now_add=True)
-  group = models.ForeignKey(Group, related_name='prikmuurpost', on_delete=models.CASCADE)
+  group = models.ForeignKey(FriendCircle, related_name='prikmuurpost', on_delete=models.CASCADE)
   postedBy = models.ForeignKey(CustomUser, related_name='prikmuurpost', on_delete=models.CASCADE)
   subject = models.CharField(max_length=45)
   text = models.TextField()

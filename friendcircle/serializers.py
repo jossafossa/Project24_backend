29c1d9a8da8d0d0fdb4057524c1d7024328b9533
Jelle Kaufmann <github@pa5am.nl>
2019-07-01 17:01:53 +0200

    Interests als string teruggeven

--- friendcircle/serializers.py
+++ friendcircle/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from . import models
+from interests.models import Interest
 
 class FriendCircleSerializer(serializers.ModelSerializer):
+    interests = serializers.SlugRelatedField(
+        many=True,
+        read_only=False,
+        slug_field='name',
+        queryset=Interest.objects.all(),
+     )
     class Meta:
         model = models.FriendCircle
         fields = ('url', 'id', 'name', 'description', 'interests', 'members', )
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from . import models
+from interests.models import Interest
 
 class UserSerializer(serializers.ModelSerializer):
+    interests = serializers.SlugRelatedField(
+        many=True,
+        read_only=False,
+        slug_field='name',
+        queryset=Interest.objects.all(),
+     )
     class Meta:
         model = models.CustomUser
         fields = ('url', 'id', 'email', 'username','interests', 'pic1', 'pic2', 'pic3', 'pic4', 'pic5', 'memberships'  )

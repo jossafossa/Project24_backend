767b3bd9cb0d3e9a628a99177753015d7e30fdfe
Jelle Kaufmann <github@pa5am.nl>
2019-07-08 14:26:59 +0200

    Voeg de groep aanmakende gebruiker direct toe aan de groep

--- friendcircle/serializers.py
+++ friendcircle/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.utils import model_meta
 from . import models
 from users.models import CustomUser
 from interests.models import Interest
@@ -13,6 +14,30 @@ class FriendCircleSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.FriendCircle
         fields = ('url', 'id', 'name', 'description', 'interests', 'members', )
+    def create(self, validated_data):
+        # Remove many-to-many relationships from validated_data.
+        # They are not valid arguments to the default `.create()` method,
+        # as they require that the instance has already been saved.
+        ModelClass = self.Meta.model
+
+        info = model_meta.get_field_info(ModelClass)
+        many_to_many = {}
+        for field_name, relation_info in info.relations.items():
+            if relation_info.to_many and (field_name in validated_data):
+                many_to_many[field_name] = validated_data.pop(field_name)
+        for x in validated_data:
+            print(x)
+        # Voeg de aanmakende gebruiker toe aan de net aangemaakte groep
+        instance = ModelClass._default_manager.create(**validated_data)
+        instance.members.add(self.context['request'].user)
+
+        # Save many-to-many relationships after the instance is created.
+        if many_to_many:
+            for field_name, value in many_to_many.items():
+                field = getattr(instance, field_name)
+                field.set(value)
+
+        return instance
 
 class FriendCircleMembershipSerializer(serializers.ModelSerializer):
     class Meta:
